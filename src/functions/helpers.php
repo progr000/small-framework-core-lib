bafb89694bd326258c69b707801e9ae2d3fc4354
Maksym Haivoronskyi <maksym.haivoronskyi@hoststar.ch>
2023-06-06 16:07:26 +0200

    - add helpers.php for common functions

---
+++ src/functions/helpers.php
@@ -0,0 +1,21 @@
+<?php
+if (! function_exists('config')) {
+    /**
+     * @return void
+     */
+    function config()
+    {
+
+    }
+}
+
+if (! function_exists('__')) {
+    /**
+     * @param string $string
+     * @return string
+     */
+    function __($string)
+    {
+        return $string;
+    }
+}
