c7dd06113b6e329a6638d113c3cb89812a317943
Maksym Haivoronskyi <maksym.haivoronskyi@hoststar.ch>
2023-06-13 12:25:55 +0200

    - added new functions into helpers

--- src/functions/helpers.php
+++ src/functions/helpers.php
@@ -141,15 +141,15 @@ if (!function_exists('css_stack')) {
      * @param array $css
      * @return string
      */
-    function css_stack(array $css)
+    function css_stack(array $css, $www_dir = null)
     {
         $str = "";
         foreach ($css as $item) {
             if (strrpos($item, '<style') !== false) {
                 $str .= $item . "\n";
             } else {
-                $filemtime = file_exists($item)
-                    ? filemtime($item)
+                $filemtime = file_exists($www_dir . "/" . $item)
+                    ? filemtime($www_dir . "/" . $item)
                     : time();
                 $str .= '<link href="' . $item . (App::$config->get('IS_DEBUG', false) ? '?v=' . $filemtime : '') . '" rel="stylesheet">' . "\n";
             }
@@ -163,15 +163,15 @@ if (!function_exists('js_stack')) {
      * @param array $js
      * @return string
      */
-    function js_stack(array $js)
+    function js_stack(array $js, $www_dir = null)
     {
         $str = "";
         foreach ($js as $item) {
             if (strrpos($item, '<script') !== false) {
                 $str .= $item . "\n";
             } else {
-                $filemtime = file_exists($item)
-                    ? filemtime($item)
+                $filemtime = file_exists($www_dir . "/" . $item)
+                    ? filemtime($www_dir . "/" . $item)
                     : time();
                 $str .= '<script src="' . $item . (App::$config->get('IS_DEBUG', false) ? '?v=' . $filemtime : '') . '"></script>' . "\n";
             }
