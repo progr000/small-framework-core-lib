e4ec7c87bc02039942b8d21b60e6500870607f67
Maksym Haivoronskyi <maksym.haivoronskyi@hoststar.ch>
2023-11-02 09:47:50 +0100

    - Relationships developing

--- src/Traits/HasRelationships.php
+++ src/Traits/HasRelationships.php
@@ -8,29 +8,53 @@ use Core\Exceptions\DbException;
 trait HasRelationships
 {
     /**
+     * @param string|null $key
+     * @return mixed
+     */
+    private function prepareKey($key)
+    {
+        $class = get_class($this);
+        if (is_null($key)) {
+            $key = $class::$_primary_key_field;
+        }
+        return $key;
+    }
+
+    /**
+     * @param ActiveRecordDriver|mixed $related
+     * @param string $foreignKey
+     * @param string|null $localKey
+     * @return ActiveRecordDriver|null
      * @throws DbException
      */
     public function hasOne($related, $foreignKey, $localKey = null)
     {
-        $class = get_class($this);
-        if (is_null($localKey)) {
-            $localKey = $class::$_primary_key_field;
-        }
-        //dd($class, $related, $foreignKey, $localKey);
-        /** @var ActiveRecordDriver $related */
+        $localKey = $this->prepareKey($localKey);
         return $related::findOne([$foreignKey => $this->{$localKey}]);
     }
 
     /**
+     * @param ActiveRecordDriver|mixed $related
+     * @param string $foreignKey
+     * @param string|null $localKey
+     * @return array|false|string
      * @throws DbException
      */
     public function hasMany($related, $foreignKey, $localKey = null)
     {
-        $class = get_class($this);
-        if (is_null($localKey)) {
-            $localKey = $class::$_primary_key_field;
-        }
-        /** @var ActiveRecordDriver $related */
+        $localKey = $this->prepareKey($localKey);
         return $related::find()->where([$foreignKey => $this->{$localKey}])->all();
+    }
+
+    /**
+     * @param ActiveRecordDriver|mixed $related
+     * @param string $foreignKey
+     * @param string|null $ownerKey
+     * @return void
+     */
+    public function belongsTo($related, $foreignKey, $ownerKey = null)
+    {
+        $ownerKey = $this->prepareKey($ownerKey);
+        return $related::findOne([$foreignKey => $this->{$ownerKey}]);
     }
 }
