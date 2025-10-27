8f7c3e191ab8bc65ca7eb112da053468d2d8630f
Maksym Haivoronskyi <maksym.haivoronskyi@hoststar.ch>
2025-10-27 14:54:00 +0100

    some helper functions are separate lib now

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -94,58 +94,11 @@ class HelpersTest extends _BaseTestCase
     /**
      * @return void
      */
-    public function testMinimize()
-    {
-        $str = <<<EOF
-<script>
-a =    1;
-alert(a); 
-</script>
-EOF;
-        $res = minimize($str);
-        $this->assertEquals($str, $res);
-        config()->set('minimize-plain-css-js', true);
-        $res = minimize($str);
-        $this->assertEquals("<script>a=1;alert(a);</script>", $res);
-    }
-
-    /**
-     * @return void
-     */
     public function testUrl()
     {
         $res = url("/index.html", ['a' => 1, 'b' => 2]);
         $this->assertEquals("/index.html?a=1&b=2", $res);
         $res = url("/index.html", "?c=3&d=4");
         $this->assertEquals("/index.html?c=3&d=4", $res);
-    }
-
-    /**
-     * @return void
-     */
-    public function testReplaceMultiSpacesAndNewLine()
-    {
-        $str = <<<EOF
-      test   string
-      
-      new   line   string
-EOF;
-        $res = replaceMultiSpacesAndNewLine($str);
-        $this->assertEquals("test string new line string", $res);
-    }
-
-    /**
-     * @return void
-     */
-    public function testSizeFormat()
-    {
-        $res = size_format(2000);
-        $this->assertEquals("1.95 KB", $res);
-        $res = size_format(2000000, 3, '-', 'KB');
-        $this->assertEquals("1953.125-KB", $res);
-        $res = size_format(2000000, 3, ' ');
-        $this->assertEquals("1.907 MB", $res);
-        $res = size_format(2000000, 3, ' ', 'KB', true);
-        $this->assertEquals("1953.125", $res);
     }
 }
